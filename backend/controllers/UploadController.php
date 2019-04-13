24533972444649e75c48f0baa325c6847c8d8c7c
toto999 <xxj8711@gmail.com>
2019-04-13 16:23:45 +0800

    add-UploadForm.php,uploadController.php;fix CORS happen

---
+++ backend/controllers/UploadController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\rest\Controller;
+use common\models\UploadForm;
+use yii\web\UploadedFile;
+
+class UploadController extends Controller
+{
+	
+	public static function allowedDomains() {
+		return [
+			//'*',                        // star allows all domains
+			'http://localhost:8080',
+			//'http://test2.example.com',
+		];
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors() {
+		
+		return array_merge(parent::behaviors(), [
+
+			//For cross-domain AJAX request
+			'corsFilter'  => [
+				'class' => \yii\filters\Cors::className(),
+				'cors'  => [
+					// restrict access to domains:
+					'Origin'                           => static::allowedDomains(),
+					'Access-Control-Request-Method'    => ['POST', 'OPTIONS'],
+					'Access-Control-Allow-Credentials' => false,
+					'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+				],
+			],
+		]);
+	}
+    public function actionIndex()
+    {
+		$model = new UploadForm();
+	
+		$request = Yii::$app->request;
+		//var_dump($request);
+        if (Yii::$app->request->isPost) {
+            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
+			//var_dump($model->imageFile);
+            if ($model->upload()) {
+                // file is uploaded successfully
+				$response = ['code' => 1000, 'imgUrl' => $model->imageFile->name];
+                return $this->serializeData($response);
+            }
+        }
+		
+        return $this->render('index', ['model' => $model]);
+    }
+	
+	
+
+}
