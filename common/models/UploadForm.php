cd7199be4e32ddc692b857036d4c4e692e75a611
toto999 <xxj8711@gmail.com>
2019-04-23 17:22:30 +0800

    upload directory move to the root path of project

--- common/models/UploadForm.php
+++ common/models/UploadForm.php
@@ -22,7 +22,7 @@ class UploadForm extends Model
     public function upload()
     {
         if ($this->validate()) {
-            $this->imageFile->saveAs('uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            $this->imageFile->saveAs('../uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
             return true;
         } else {
             return false;
